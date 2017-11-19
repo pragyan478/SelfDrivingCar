2dd6b6b56f39f055b553802113d93a0afcf43aed
semonemo <32355194+semonemo@users.noreply.github.com>
2017-11-19 17:21:25 +0900

    Update Vector.cpp

--- PracticeCplusplus/Vector.cpp
+++ PracticeCplusplus/Vector.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
 
     vector<float> example;
-    
+    /*part 1*/
     //Here is a program that initializes a vector and then uses a for loop to
     //populate the vector with values. Then another for loop reads out the vector values.
     for (int i = 0; i < 5; i++) {
@@ -17,7 +17,7 @@ int main() {
         cout << example[i] << endl;
     }
 
-
+    /*part2*/
     // assign 5 floats with value 10
     example.assign(5,10.0);
 
@@ -38,6 +38,59 @@ int main() {
     for (int i = 0; i < example.size(); i++) {
         cout << example[i] << endl;
     }
+    
+    
+    /*part 3*/
+    vector<int> exampleone (5);
+    vector<int> exampletwo (5);
+    vector<int> examplesum (5);
+
+    exampleone[0] = 2;
+    exampleone[1] = 6;
+    exampleone[2] = 25;
+    exampleone[3] = 1;
+    exampleone[4] = 18;
+
+    exampletwo[0] = 3;
+    exampletwo[1] = 19;
+    exampletwo[2] = 8;
+    exampletwo[3] = 12;
+    exampletwo[4] = 191;
+
+    cout << "vector one ";
+
+    // print out the first vector
+    for (int i = 0; i < exampleone.size(); i++) {
+        cout << exampleone[i] << " ";
+    }
+
+    // create a new line in the terminal
+    cout << endl;
+
+    cout << "vector two ";
+
+    // print out the second vector
+    for (int i = 0; i < exampletwo.size(); i++) {
+        cout << exampletwo[i] << " ";
+    }
+
+    // create a new line in the terminal
+    cout << endl;
+
+    cout << "vector sum ";
+
+    //add the vectors together
+    for (int i = 0; i < exampleone.size(); i++) {
+        examplesum[i] = exampleone[i] + exampletwo[i];
+    }
+
+    // print out the vector
+    for (int i = 0; i < examplesum.size(); i++) {
+        cout << examplesum[i] << " ";
+    }
+
+    // create a new line in the terminal
+    cout << endl;
 
 
     return 0;
