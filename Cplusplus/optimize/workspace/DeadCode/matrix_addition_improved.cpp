1a137bb4feb981f4645444a46d05d3738661c49a
semonemo <95gusals@naver.com>
2017-11-26 06:04:59 +0900

    2seconds faster

--- Cplusplus/optimize/workspace/DeadCode/matrix_addition_improved.cpp
+++ Cplusplus/optimize/workspace/DeadCode/matrix_addition_improved.cpp
@@ -3,36 +3,24 @@
 using namespace std;
 
 vector < vector <int> > matrix_addition_improved (vector < vector <int> > matrixa, vector < vector <int> > matrixb) {
-    
-    // store the number of rows and columns in the matrices
-    vector<int>::size_type rows_a = matrixa.size();
-    vector<int>::size_type rows_b = matrixb.size();
-    vector<int>::size_type cols_a = matrixa[0].size();
-    vector<int>::size_type cols_b = matrixb[0].size();
-
-    // default zero vector
-    vector <vector <int> > default_vector(rows_a,vector<int>(cols_b));
-
+   
+    int row_a = matrixa.size();
+    int row_b = matrixb.size();
+    int col_a = matrixa[0].size();
+    int col_b = matrixb[0].size();
     // if both matrices have the same size, calculate and return the sum
     // otherwise check if the number of rows and columns are not equal and return a matrix of zero
-    if (rows_a == rows_b && cols_a == cols_b) {
+    if (row_a == row_b && col_a == col_b) {
     
-        vector < vector <int> > matrix_sum(matrixa.size(), vector<int>(matrixa[0].size()));
-        
-        for (unsigned int i = 0; i < matrixa.size(); i++) {
-            for (unsigned int j = 0; j < matrixa[0].size(); j++) {
-                matrix_sum[i][j] = matrixa[i][j] + matrixb[i][j];
+        for (int i = 0; i < row_a; i++) {
+            for (int j = 0; j < col_a; j++) {
+                matrixa[i][j] += matrixb[i][j];
             }
         }
-        return matrix_sum;
-    }
-    else if (rows_a != rows_b) {
-        return default_vector;
-    }
-    else if (cols_a != cols_b) {
-        return default_vector;
+        return matrixa;
     }
     else {
+        vector <vector<int> > default_vector(matrixa.size(), vector<int> (matrixa[0].size()));
         return default_vector;
     }
     
