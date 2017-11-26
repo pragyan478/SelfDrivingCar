f31ddccde12e9c14bc75384736903c43291d343e
semonemo <95gusals@naver.com>
2017-11-26 16:08:20 +0900

    2 times faster

--- Cplusplus/optimize/workspace/Static/blur_factor_improved.cpp
+++ Cplusplus/optimize/workspace/Static/blur_factor_improved.cpp
@@ -3,78 +3,77 @@
 using namespace std;
 
 vector < vector <float> > blur_factor_improved() {
-    
+
     // 2D vector reprenting the blur filter
     vector < vector <float> > window;
+    window.reserve(3);
     vector <float> row;
-    float blurring, center, corner, adjacent;
-    
+    row.reserve(3);
+
     // calculate blur factors
-    blurring = .12;
-    center = 1.0 - blurring;
-    corner = blurring / 12.0;
-    adjacent = blurring / 6.0;
-    
+    static float blurring = .12;
+    static float center = 1.0 - blurring;
+    static float corner = blurring / 12.0;
+    static float adjacent = blurring / 6.0;
+
     int i, j;
     float val;
-    
-    // create the blur window
-    for (i=0; i<3; i++) {
-        row.clear();
-        for (j=0; j<3; j++) {
-            switch (i) {
-                case 0:
-                    switch (j) {
-                        case 0:
-                            val = corner;
-                            break;
-                            
-                        case 1:
-                            val = adjacent;
-                            break;
-                            
-                        case 2:
-                            val = corner;
-                            break;
-                    }
-                    break;
-                    
-                case 1:
-                    switch (j) {
-                        case 0:
-                            val = adjacent;
-                            break;
-                            
-                        case 1:
-                            val = center;
-                            break;
-                            
-                        case 2:
-                            val = adjacent;
-                            break;
-                    }
-                    break;
-                    
-                case 2:
-                    switch(j) {
-                        case 0:
-                            val = corner;
-                            break;
-                            
-                        case 1:
-                            val = adjacent;
-                            break;
-                            
-                        case 2:
-                            val = corner;
-                            break;
-                    }
-                    break;
-            }
-            row.push_back(val);
+
+    for (j=0; j<3; j++) {
+        switch (j) {
+            case 0:
+                val = corner;
+                break;
+
+            case 1:
+                val = adjacent;
+                break;
+
+            case 2:
+                val = corner;
+                break;
         }
-        window.push_back(row);
+        row.push_back(val);
+    } 
+    window.push_back(row);
+
+    row.clear();
+    for (j=0; j<3; j++) {
+        switch (j) {
+            case 0:
+                val = adjacent;
+                break;
+
+            case 1:
+                val = center;
+                break;
+
+            case 2:
+                val = adjacent;
+                break;
+        }
+        row.push_back(val);
     }
+    window.push_back(row);
+    row.clear();
+
+    for(j=0 ; j<3 ; j++){
+        switch(j) {
+            case 0:
+                val = corner;
+                break;
+
+            case 1:
+                val = adjacent;
+                break;
+
+            case 2:
+                val = corner;
+                break;
+        }
+        row.push_back(val);
+    }
+    window.push_back(row);
     return window;
 }
 
