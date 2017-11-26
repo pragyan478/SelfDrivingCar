168b5fd8d4a6a0746f5efae35e42b1413d5491eb
semonemo <95gusals@naver.com>
2017-11-26 15:12:31 +0900

    2 times faster

--- Cplusplus/optimize/workspace/IntermediateVariables/scalar_multiply_improved.cpp
+++ Cplusplus/optimize/workspace/IntermediateVariables/scalar_multiply_improved.cpp
@@ -5,19 +5,14 @@ vector< vector<int> > scalar_multiply_improved(vector< vector<int> > matrix, int
     
   	// OPTIMIZATION: Instead of creating a new variable
     // called resultmatrix, update the input matrix directly
-    vector< vector<int> > resultmatrix;
-    vector<int> new_row;
     
     vector<int>::size_type num_rows = matrix.size();
     vector<int>::size_type num_cols = matrix[0].size();
     
     for (int i = 0; i < num_rows; i++) {
-        new_row.clear();
-        for (int j = 0; j < num_cols; j++) {
-            new_row.push_back(matrix[i][j] * scalar);
-        }
-        resultmatrix.push_back(new_row);
+        for (int j = 0; j < num_cols; j++) 
+            matrix[i][j] *= scalar;
     }
     
-    return resultmatrix;
+    return matrix;
 }
